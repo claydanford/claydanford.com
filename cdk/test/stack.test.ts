f92d13e0f16f9bc5007a1e9d172c7a6df0d6422a
Clay Danford <clay_danford@mckinsey.com>
2022-02-18 11:16:42 -0600

    cdk tests

--- cdk/test/stack.test.ts
+++ cdk/test/stack.test.ts
@@ -1,16 +1,28 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as Cdk from '../lib/cdk-stack';
+import * as cdk from 'aws-cdk-lib'
+import { Template } from 'aws-cdk-lib/assertions'
+import * as TestStack from '../lib/stack'
 
-// example test. To run these tests, uncomment this file along with the
-// example resource in lib/cdk-stack.ts
-test('SQS Queue Created', () => {
-  //   const app = new cdk.App();
-  //     // WHEN
-  //   const stack = new Cdk.CdkStack(app, 'MyTestStack');
-  //     // THEN
-  //   const template = Template.fromStack(stack);
-  //   template.hasResourceProperties('AWS::SQS::Queue', {
-  //     VisibilityTimeout: 300
-  //   });
+test('Stack Resources Created', () => {
+  const app = new cdk.App()
+  // WHEN
+  const stack = new TestStack.ClayDanfordDotCom(app, 'MyTestStack')
+  // THEN
+  const template = Template.fromStack(stack)
+
+  template.resourceCountIs("AWS::Route53::HostedZone", 1)
+  template.resourceCountIs("AWS::CertificateManager::Certificate", 1)
+  template.resourceCountIs("AWS::S3::Bucket", 1)
+  template.resourceCountIs("AWS::S3::BucketPolicy", 1)
+  template.resourceCountIs("AWS::CloudFront::CloudFrontOriginAccessIdentity", 1)
+  template.resourceCountIs("AWS::CloudFront::Distribution", 1)
+  template.resourceCountIs("AWS::Route53::RecordSet", 2)
+  template.resourceCountIs("AWS::SSM::Parameter", 2)
+  template.resourceCountIs("AWS::AppSync::GraphQLApi", 1)
+  template.resourceCountIs("AWS::AppSync::GraphQLSchema", 1)
+  template.resourceCountIs("AWS::AppSync::ApiKey", 1)
+  template.resourceCountIs("AWS::IAM::Role", 1)
+  template.resourceCountIs("AWS::IAM::Policy", 1)
+  template.resourceCountIs("AWS::AppSync::DataSource", 1)
+  template.resourceCountIs("AWS::AppSync::Resolver", 2)
+  template.resourceCountIs("AWS::DynamoDB::Table", 1)
 })
